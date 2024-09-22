7d813fcf084f85ec2a7f46358b1d40aa746c6dda
xingxingRealzyx <1064280153@qq.com>
2024-09-23 02:29:39 +0800

    feat(tinker): 可以训练了，但是要逐渐调整奖励

--- legged_gym/envs/tinker/tinker.py
+++ legged_gym/envs/tinker/tinker.py
@@ -21,11 +21,14 @@ class Tinker(LeggedRobot):
     def __init__(self, cfg: LeggedRobotCfg, sim_params, physics_engine, sim_device, headless):
         super().__init__(cfg, sim_params, physics_engine, sim_device, headless)
         self.base_euler_xyz = get_euler_xyz_tensor(self.base_quat)
-
+        self.ref_dof_pos = torch.zeros_like(self.dof_pos)  # 步态生成器-生成的参考姿势
     def compute_observations(self):
         """ Computes observations
 
         """
+        self.base_euler_xyz = get_euler_xyz_tensor(self.base_quat)
+        # self.obs_buf shape: [num_envs, num_observations] , 这里就是 [4096, 39]
+        # dim = -1 表示在最后一个维度上进行拼接
         self.obs_buf = torch.cat((  
             self.commands[:, :3] * self.commands_scale,  # 3 命令
             self.base_ang_vel * self.obs_scales.ang_vel,  # 3 基座角速递
@@ -34,16 +37,92 @@ class Tinker(LeggedRobot):
             self.dof_vel * self.obs_scales.dof_vel,  # 10 所有自由度的角速度
             self.actions * self.cfg.control.action_scale,  # 10 上一轮输出的action
             ),dim=-1)
-
+        
         # add perceptive inputs if not blind
         if self.cfg.terrain.measure_heights:
+            # print("0------------" + str(self.obs_buf.size()))
+            # print("1------------" + str(self.root_states[:, 2].size()))
+            # print("2------------" + str(self.root_states[:, 2].unsqueeze(1).size()))
+            # print("3------------" + str(self.measured_heights.size()))
+
+            # self.root_states shape: [num_envs, 13]
+            # 取 z 轴, self.root_states[:, 2]的形状是[num_envs],unsqueeze(1)之后的形状是 [num_envs, 1]
             heights = torch.clip(self.root_states[:, 2].unsqueeze(1) - 0.5 - self.measured_heights, -1, 1.) * self.obs_scales.height_measurements
+            # print("0-height-----------" + str(heights.size()))
+
             self.obs_buf = torch.cat((self.obs_buf, heights), dim=-1) # 1
+            # print("1------------" + str(self.obs_buf.size()))
+
         # add noise if needed
         if self.add_noise:
+            # print("2------------" + str(self.obs_buf.size()))
             self.obs_buf += (2 * torch.rand_like(self.obs_buf) - 1) * self.noise_scale_vec
+    def check_termination(self):
+        super().check_termination()
+        measured_heights = torch.sum(self.rigid_state[:, self.feet_indices, 2], dim=1) / 2
+        base_height = self.root_states[:, 2] - (measured_heights - 0.05)
+        self.reset_buf2 = base_height < self.cfg.asset.terminate_body_height
+        # self.reset_buf2 = self.root_states[:, 2] < self.cfg.asset.terminate_body_height  # 0.3!!!!!!!!!!!!!!!!!
+        self.reset_buf |= self.reset_buf2
+
+    # ------------------------ rewards --------------------------------------------------------------------------------
 
     def _reward_no_fly(self):
         contacts = self.contact_forces[:, self.feet_indices, 2] > 0.1
         single_contact = torch.sum(1.*contacts, dim=1)==1
         return 1.*single_contact # 奖励只有一只脚接触地面的情况
+
+    def _reward_target_joint_pos_l(self):
+        """
+        Calculates the reward for keeping joint positions close to default positions, with a focus
+        on penalizing deviation in yaw and roll directions. Excludes yaw and roll from the main penalty.
+        """
+        joint_diff_l = torch.sum((self.dof_pos[:, 5:10] - self.ref_dof_pos[:, 5:10]) ** 2, dim=1)
+        imitate_reward = torch.exp(-7 * joint_diff_l)
+        return imitate_reward # 奖励左侧关节幅度小的情况
+    
+    def _reward_target_joint_pos_r(self):
+        """
+        Calculates the reward for keeping joint positions close to default positions, with a focus
+        on penalizing deviation in yaw and roll directions. Excludes yaw and roll from the main penalty.
+        """
+        joint_diff_r = torch.sum((self.dof_pos[:, 0:5] - self.ref_dof_pos[:, 0:5]) ** 2, dim=1)
+        imitate_reward = torch.exp(-7*joint_diff_r)  # positive reward, not the penalty
+        return imitate_reward # 奖励右侧关节幅度小的情况
+    
+    def _reward_orientation(self):
+        # 奖励机器人倾角小
+        return torch.exp(-10. * torch.sum(torch.square(self.projected_gravity[:, :2]), dim=1))
+    
+    def _reward_tracking_lin_x_vel(self):
+        # Tracking of linear velocity commands (xy axes)
+        lin_vel_error = torch.abs(self.commands[:, 0] - self.base_lin_vel[:, 0])
+        return torch.exp(-lin_vel_error / self.cfg.rewards.tracking_sigma)
+
+    def _reward_tracking_lin_y_vel(self):
+        # Tracking of linear velocity commands (xy axes)
+        lin_vel_error = torch.abs(self.commands[:, 1] - self.base_lin_vel[:, 1])
+        return torch.exp(-lin_vel_error / self.cfg.rewards.tracking_sigma)
+
+    def _reward_target_ankle_pos(self):
+        """
+        Calculates the reward for keeping joint positions close to default positions, with a focus
+        on penalizing deviation in yaw and roll directions. Excludes yaw and roll from the main penalty.
+        """
+        diff = torch.abs(self.dof_pos[:, 4:6] - self.ref_dof_pos[:, 4:6])
+        diff += torch.abs(self.dof_pos[:, 10:] - self.ref_dof_pos[:, 10:])
+        ankle_imitate_reward = torch.exp(-20*torch.sum(diff, dim=1))  # positive reward, not the penalty
+        return ankle_imitate_reward
+
+    def _reward_feet_distance(self):
+        """
+        Calculates the reward based on the distance between the feet. Penilize feet get close to each other or too far away.
+        """
+        foot_pos = self.rigid_state[:, self.feet_indices, :2]
+        # torch.norm 
+        foot_dist = torch.norm(foot_pos[:, 0, :] - foot_pos[:, 1, :], dim=1)
+        fd = self.cfg.rewards.min_dist # 0.2
+        max_df = self.cfg.rewards.max_dist # 0.5
+        d_min = torch.clamp(foot_dist - fd, -0.5, 0.)
+        d_max = torch.clamp(foot_dist - max_df, 0, 0.5)
+        return (torch.exp(-torch.abs(d_min) * 100) + torch.exp(-torch.abs(d_max) * 100)) / 2
--- legged_gym/envs/tinker/tinker_config.py
+++ legged_gym/envs/tinker/tinker_config.py
@@ -2,17 +2,18 @@ from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobot
 
 class TinkerRoughCfg( LeggedRobotCfg ):
     class env( LeggedRobotCfg.env):
-        num_envs = 1024 
+        num_envs = 4096 
         num_observations = 39
         num_actions = 10
+        env_spacing = 1.
 
     class terrain( LeggedRobotCfg.terrain):
         # measured_points_x = [-0.5, -0.4, -0.3, -0.2, -0.1, 0., 0.1, 0.2, 0.3, 0.4, 0.5] # 1mx1m rectangle (without center line)
         # measured_points_y = [-0.5, -0.4, -0.3, -0.2, -0.1, 0., 0.1, 0.2, 0.3, 0.4, 0.5]
         mesh_type = 'plane' # "heightfield" # none, plane, heightfield or trimesh
-        measure_heights = False
+        measure_heights = False # 测量高度
     class init_state( LeggedRobotCfg.init_state ):
-        pos = [0.0, 0.0, 1.] # x,y,z [m]
+        pos = [0.0, 0.0, 0.5] # x,y,z [m]
         default_joint_angles = { # = target angles [rad] when action = 0.0
             'joint_l_yaw': 0., 
             'joint_l_roll': 0., 
@@ -30,47 +31,70 @@ class TinkerRoughCfg( LeggedRobotCfg ):
 
     class control( LeggedRobotCfg.control ):
         # PD Drive parameters:
-        stiffness = {   'yaw': 100.0, 'roll': 100.0, 'pitch': 100.0, 
-                        'knee': 200., 'ankle': 40.}  # [N*m/rad]
-        damping = { 'yaw': 3.0, 'roll': 3.0, 'pitch' : 3.0,
-                    'knee': 6., 'ankle': 6 }  # [N*m*s/rad]     # [N*m*s/rad]
+        stiffness = { 'joint': 100.0 }  # [N*m/rad]
+        damping = { 'joint': 3.0 }  # [N*m*s/rad]
         # action scale: target angle = actionScale * action + defaultAngle
-        action_scale = 0.5
+        action_scale = 0.1
         # decimation: Number of control action updates @ sim DT per policy DT
-        decimation = 4
-        
+        decimation = 2
+
+    class normalization(LeggedRobotCfg.normalization):
+        class obs_scales(LeggedRobotCfg.normalization.obs_scales):
+            lin_vel = 1.0
+            ang_vel = 0.25
+            dof_pos = 1.0
+            dof_vel = 0.05
+            height_measurements = 5.0
+        clip_observations = 50.
+        clip_actions = 100.
+
+    class commands(LeggedRobotCfg.commands):
+        step_joint_offset = 0.30  # rad
+        step_freq = 1.5  # HZ （e.g. cycle-time=0.66）
+
+        class ranges(LeggedRobotCfg.commands.ranges):
+            #lin_vel_x = [-0.3, 0.5]  # min max [m/s]
+            #lin_vel_y = [-0.0, 0.0]   # min max [m/s]
+            #ang_vel_yaw = [-0.3, 0.3]    # min max [rad/s]
+            #heading = [-3.14, 3.14]
+            lin_vel_x = [-0.0, 0.7]  # min max [m/s]
+            lin_vel_y = [-0.0, 0.0]  # min max [m/s]
+            ang_vel_yaw = [-0.0, 0.0]  # min max [rad/s]
+            heading = [-0, 0]
+        heading_command = False  # if true: compute ang vel command from heading error
+
     class asset( LeggedRobotCfg.asset ):
         file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/tinker/urdf/tinker_urdf.urdf'
-        name = "tinker"
-        # foot_name = 'foot'
+        name = "foot"
+        foot_name = 'foot' # 脚的刚体的名字
         # terminate_after_contacts_on = ['pelvis']
         flip_visual_attachments = False # 模型显示不正确可能是因为这个
-        self_collisions = 1 # 1 to disable, 0 to enable...bitwise filter
+        self_collisions = 0 # 1 to disable, 0 to enable...bitwise filter
+        terminate_body_height = 0.4
 
     class rewards( LeggedRobotCfg.rewards ):
         soft_dof_pos_limit = 0.95
         soft_dof_vel_limit = 0.9
         soft_torque_limit = 0.9
         max_contact_force = 300.
-        only_positive_rewards = False
+        only_positive_rewards = True
+        min_dist = 0.2 # 两只脚的最小距离
+        max_dist = 0.5 # 两只脚的最大距离
         class scales( LeggedRobotCfg.rewards.scales ):
-            termination = -200.
-            tracking_ang_vel = 1.0
-            torques = -5.e-6
-            dof_acc = -2.e-7
-            lin_vel_z = -0.5
-            feet_air_time = 5.
-            dof_pos_limits = -1.
-            no_fly = 0.25
-            dof_vel = -0.0
-            ang_vel_xy = -0.0
-            feet_contact_forces = -0.
+            termination = -5.
+            target_joint_pos_r = 10.0  # 3.  reference joint imitation reward
+            target_joint_pos_l = 10.0
+            orientation = 10.0  # 
+            feet_distance = 1.0
 
 class TinkerRoughCfgPPO( LeggedRobotCfgPPO ):
-    
+    init_noise_std = 0.12
+    actor_hidden_dims = [512, 256, 128]
+    critic_hidden_dims = [512, 256, 128]
+    activation = 'tanh'  # can be elu, relu, selu, crelu, lrelu, tanh, sigmoid
     class runner( LeggedRobotCfgPPO.runner ):
         run_name = ''
-        experiment_name = 'rough_tinker'
+        experiment_name = 'tinker'
 
     class algorithm( LeggedRobotCfgPPO.algorithm):
         entropy_coef = 0.01
